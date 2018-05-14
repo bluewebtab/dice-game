9ce9f8caa918501e3e8b7a8f147a24dc344c855a
Ronny Gutierrez <ronny.icne@gmail.com>
2018-05-13 20:45:26 -0700

    8:45pm

--- challenges.js
+++ challenges.js
@@ -1,19 +1,20 @@
-/*
-GAME RULES:
+////*
+//3 Challenges
+//
+//1.A player loses his entire score when he rolls two 6 in a row. After that, it's the next player's turn.(Hint:"Always save the previous dice roll in a separate variable")
+//
+//2. Add an input field to the html where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the value property in Js, This is a good opportunity to use google to figure this out.)
+//3.Add another dice to the fame, so that there are two dices now. The player loses his current score when of them is a 1. (Hint: you will need Css to position the second dice, so take a look at the css code for the first one.)
+//*/
+//
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
 
-*/
 //Global scope allows these variables to be used by each functions
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-
+var lastDice;
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
@@ -30,8 +31,15 @@ var diceDOM =  document.querySelector('.dice');
 diceDOM.style.display = 'block';
  diceDOM.src = 'dice-' + dice + '.png';
     
-    //3. Update the round score If the rolled number was Not a 1.
-    if(dice !== 1){
+   
+    if(dice === 6 && lastDice ===6){
+//       Player loses score 
+        scores[activePlayer] = 0;
+        document.querySelector('#score-' + activePlayer).textContent = 
+        '0';
+        nextPlayer();
+      //3. Update the round score If the rolled number was Not a 1.   
+    }else if (dice !== 1){
         //Add score
         
         roundScore += dice;
@@ -41,6 +49,8 @@ diceDOM.style.display = 'block';
         nextPlayer();
         
     }
+        
+         lastDice = dice;
     }
     
 });
@@ -105,16 +115,6 @@ document.getElementById('name-1').textContent = 'Player 2';
  document.querySelector('.player-1-panel').classList.remove('active');
  document.querySelector('.player-0-panel').classList.add('active');
 }
-
-/*
-3 Challenges
-
-1.A player loses his entire score when he rolls two 6 in a row. After that, it's the next player's turn.(Hint:"Always save the previous dice roll in a separate variable")
-
-2. Add an input field to the html where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the value property in Js, This is a good opportunity to use google to figure this out.)
-3.Add another dice to the fame, so that there are two dices now. The player loses his current score when of them is a 1. (Hint: you will need Css to position the second dice, so take a look at the css code for the first one.)
-*/
-
 
 
 
